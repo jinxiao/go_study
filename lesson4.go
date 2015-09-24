91a7982e8318cd5f64b0bcc7b4c38c2287d95d5b
jinxiao <jinxiao2010@gmail.com>
2015-09-24 10:22:19 +0800

    some practices

---
+++ lesson4.go
@@ -0,0 +1,22 @@
+package main
+import (
+	"fmt"
+)
+//const a int = 65
+/*const b = 'A'
+const (
+	d =1
+	e
+	f
+)*/
+const (
+	a = 'A'
+	b = iota
+	c
+	d
+)
+func main(){
+	fmt.Println(a,b,c,d)
+	//fmt.Println(d,"",e,"",f)
+	fmt.Println(a &^ c)
+}
