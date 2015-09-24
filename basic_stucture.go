6792e835be0f35849fab2df7504c072b1a45115f
jinxiao <jinxiao2010@gmail.com>
2015-09-24 10:22:19 +0800

    some practices

---
+++ basic_stucture.go
@@ -0,0 +1,20 @@
+package main
+
+//import "fmt"
+import "fmt"
+
+const PI = 3.14
+
+//var name = "jinxiao"
+var (
+	name1 = "jinxiao"
+	name3 = "2b"
+)
+
+type newtype int
+
+func main() {
+	fmt.Println(name3+" ")
+	var b int = 11
+	fmt.Println(name1,b)
+}
